7442a539feaa0b2f3915cfedda22f89186cc2113
junoyoon <junoyoon@gmail.com>
2023-07-18 01:59:29 +0900

    add transactional

--- src/integration/kotlin/org/springframework/samples/petclinic/practice/RestAssuredMockMvcTest.kt
+++ src/integration/kotlin/org/springframework/samples/petclinic/practice/RestAssuredMockMvcTest.kt
@@ -1,4 +1,4 @@
-package org.springframework.samples.petclinic.restassured
+package org.springframework.samples.petclinic.practice
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.kotest.inspectors.forAll
@@ -12,12 +12,16 @@ import io.restassured.config.ObjectMapperConfig
 import io.restassured.http.ContentType
 import io.restassured.mapper.ObjectMapperType
 import io.restassured.module.mockmvc.RestAssuredMockMvc
+import io.restassured.module.mockmvc.kotlin.extensions.Extract
 import io.restassured.module.mockmvc.kotlin.extensions.Given
 import io.restassured.module.mockmvc.kotlin.extensions.Then
 import io.restassured.module.mockmvc.kotlin.extensions.When
 import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -26,10 +30,13 @@ import org.springframework.http.MediaType
 import org.springframework.samples.petclinic.fixtures.Fixtures
 import org.springframework.samples.petclinic.restassured.extensions.extractBodyAs
 import org.springframework.samples.petclinic.rest.dto.OwnerDto
+import org.springframework.samples.petclinic.restassured.extensions.ExtractBodyAs
+import org.springframework.samples.petclinic.restassured.extensions.noOp
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.transaction.annotation.Transactional
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @Transactional
 @WithMockUser(roles = ["OWNER_ADMIN"])
 @AutoConfigureMockMvc
@@ -52,74 +59,52 @@ class RestAssuredMockMvcTest(
         )
     }
 
-    @Test
-    fun testOne() {
-        Given {
-            contentType(MediaType.APPLICATION_JSON)
-            headers("key1", "value1", "key2", "value2")
-        } When {
-            get("/api/owners/1")
-        } Then {
-            status(HttpStatus.OK)
-            body("pets[0].name", equalTo("Leo"))
-            extractBodyAs<OwnerDto>().shouldNotBeNull()
-            log().all()
-        }
-    }
-
-    @Test
-    fun testAll() {
-        Given {
-            contentType(MediaType.APPLICATION_JSON)
-        } When {
-            get("/api/owners")
-        } Then {
-            status(HttpStatus.OK)
-            extractBodyAs<List<OwnerDto>>().should {
-                it shouldNotHaveSize 0
-                it.forAll { owner ->
-                    owner.firstName shouldNotBe null
-                    owner.pets shouldNotBe  null
-                }
-            }
-            log().all()
-        }
-    }
-
     // mock mvc 는 트랜잭션 가능
+    @Order(1) // 이게 먼저 수행되어야 함으로 순서 지정
     @Transactional
     @Test
-    fun testPost() {
+    fun testPostAndGet() {
+        // lastName이 이 hello 인 사용자 생성
         val newOwner = Fixtures.giveMeOneFreshOwner()
+        var id : Int = 0
         Given {
-            headers("key1", "value1", "key2", "value2")
             body(newOwner)
-        } When  {
+            log().all()
+        } When {
             post("/api/owners")
-        } Then  {
+        } Then {
             status(HttpStatus.CREATED)
             extractBodyAs<OwnerDto>().should { dto ->
-                dto.lastName shouldBe newOwner.lastName
-                dto.firstName shouldBe newOwner.firstName
-                dto.id shouldNotBe null
-                dto.pets.shouldNotBeNull() shouldHaveSize 0
+                id = dto.id!!
+                // ...
             }
+        }
+
+        // id 로 가져올때 잘 가져와 지는지 확인
+        Given {
             log().all()
+        } When {
+            get("/api/owners/$id")
+        } Then {
+            log().all()
+            status(HttpStatus.OK)
+            extractBodyAs<OwnerDto>().should { dto ->
+                dto.lastName shouldBe newOwner.lastName
+            }
         }
     }
 
-    @Disabled("mockmvc 는 webflux 를 인식하지 못함")
+    @Order(2)
     @Test
-    fun testWebFlux() {
+    fun testFindByLastNameHello() {
+        // lastName이 이 hello 인 사용자 탐색해봄
+        val lastName = "hello"
         Given {
-            contentType(MediaType.APPLICATION_JSON)
-        } When {
-            get("/api/owners-webflux/1")
-        } Then {
-            status(HttpStatus.OK)
-            body("$.pets[*].name", equalTo("Leo"))
-            extractBodyAs<OwnerDto>().shouldNotBeNull()
             log().all()
+        } When {
+            get("/api/owners?lastName=$lastName")
+        } Then {
+
         }
     }
 }
--- src/integration/kotlin/org/springframework/samples/petclinic/restassured/extensions/RestAssuredExtensions.kt
+++ src/integration/kotlin/org/springframework/samples/petclinic/restassured/extensions/RestAssuredExtensions.kt
@@ -1,10 +1,13 @@
 package org.springframework.samples.petclinic.restassured.extensions
 
 import io.restassured.common.mapper.TypeRef
+import io.restassured.module.mockmvc.response.MockMvcResponse
 import io.restassured.module.mockmvc.response.ValidatableMockMvcResponse
+import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification
 import io.restassured.module.webtestclient.response.ValidatableWebTestClientResponse
 import io.restassured.module.webtestclient.specification.WebTestClientRequestSender
 import io.restassured.module.webtestclient.specification.WebTestClientRequestSpecification
+import io.restassured.response.ExtractableResponse
 import io.restassured.response.ValidatableResponse
 import io.restassured.specification.RequestSpecification
 
@@ -23,6 +26,12 @@ inline fun <reified T : Any> ValidatableWebTestClientResponse.extractBodyAs() : 
 
 inline fun RequestSpecification.noOp() : RequestSpecification = this
 
+inline fun MockMvcRequestSpecification.noOp() : MockMvcRequestSpecification = this
+
 inline fun WebTestClientRequestSpecification.whenever() : WebTestClientRequestSender {
     return this.`when`()
 }
+
+inline  infix fun <reified T : Any> ValidatableMockMvcResponse.ExtractBodyAs(
+    block: ExtractableResponse<MockMvcResponse>.() -> T
+): T = extract().run(block)
